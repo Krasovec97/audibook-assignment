3b971eeb7c3965d8eac5660a002c8300836d318f
Mitja Krašovec <mitjakrasovec1@gmail.com>
2022-07-29 21:07:10 +0200

    Add first Ajax implementation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,25 @@
 import './bootstrap';
 
 import '../sass/app.scss'
+
+$("#loading").hide();
+
+$('#submitBtn').on('click',function () {
+    let request = $.ajax({
+        url: "http://localhost:8000/api/application",
+        type: "POST",
+        data: $('#applicationForm').serialize(),
+        dataType: "json"
+    });
+
+    request.done(function (msg) {
+        console.log('Success: ' + msg.code);
+    });
+
+    request.fail(function (msg){
+        console.log('Error: ' + msg);
+    });
+});
+
+
+
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Styles -->
     <link href="{{ asset('css/app.scss') }}" rel="stylesheet">
 
@@ -18,10 +19,10 @@
 <body class="bg-black bg-opacity-10">
 <div class="px-4 py-3 my-5 text-center bg-white col-5 d-flex flex-column m-auto rounded-4">
     <img class="pt-2 mb-2 justify-content-start" src="{{ asset('img/logo.svg') }}" alt="Site Logo" width="150" height="50">
-    <div class="col-10 mx-auto p-3">
+    <div class="position-relative col-10 mx-auto p-3">
         <h1 class="display-6 mx-auto fw-bold">Job Application Form</h1>
         <div class="pt-4 d-sm-flex justify-content-sm-center">
-            <form class="col-8 pb-2">
+            <form id="applicationForm" class="col-8 pb-2">
                 <div class="text-start">
                     <div class="mb-3">
                         <label for="full_name" class="form-label required">Full Name</label>
@@ -29,7 +30,7 @@
                     </div>
                     <div class="mb-3">
                         <label for="gender" class="form-label required">Gender</label>
-                        <select required name="gender" class="form-select">
+                        <select required name="gender" id="gender" class="form-select">
                             <option selected>Choose...</option>
                             <option value="male">Male</option>
                             <option value="female">Female</option>
@@ -38,19 +39,22 @@
                     </div>
                     <div class="mb-3">
                         <label for="email" class="form-label required">Email Address</label>
-                        <input required type="email" name="email" class="form-control">
+                        <input required type="email" name="email" id="email" class="form-control">
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label required">Date of birth</label>
-                        <input required type="date" class="form-control">
+                        <input required type="date" name="date_of_birth" id="date" class="form-control">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label required">Link your CV or LinkedIn</label>
-                        <input required type="text" class="form-control">
+                        <label for="link" class="form-label required">Link your CV or LinkedIn</label>
+                        <input required type="text" name="link" id="link" class="form-control">
                     </div>
                 </div>
-            <button type="submit" action="POST" class="btn btn-primary btn-lg w-50 px-4 ms-auto">Submit</button>
+
+                <button id="submitBtn" type="button" class="btn btn-primary btn-lg w-50 px-4 ms-auto">Submit</button>
+                <div class="spinner-grow text-primary" id="loading" role="status"></div>
             </form>
+
         </div>
     </div>
 </div>
